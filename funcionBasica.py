13e9ed681a9eab25eeed88fcf0b478fd61f04c1d
Aldo299 <tuemail@example.com>
2025-02-18 13:24:01 -0600

    segundo commit

--- funcionBasica.py
+++ funcionBasica.py
@@ -1,5 +1,5 @@
 def operacion_matematica(a, b, c):
-    resultado = (a ** 2 + b ** 2) * c
+    resultado = (a ** 2 + b ** 2) * c +2 
     return resultado
 
 resultado = operacion_matematica(2, 3, 4)
