c9e5b7e375f249c0522ab97c2d85141ccd55e476
Paulo Valenzuela <paulovalensuela@hotmail.com>
2025-02-18 14:55:26 -0600

    Operación de suma

--- funcionBasica.py
+++ funcionBasica.py
@@ -2,5 +2,9 @@ def operacion_matematica(a, b, c):
     resultado = (a ** 2 + b ** 2) * c +2 
     return resultado
 
-resultado = operacion_matematica(2, 3, 4)
-print("El resultado es:", resultado)  
+def suma(a,b):
+    operacion = a+b
+    return operacion
+
+print("El resultado de aldo es:", operacion_matematica(2, 3, 4))  
+print("El resultado de Paulo es:", suma(5+6))
